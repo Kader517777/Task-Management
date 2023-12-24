c499fc0d198299c3b142d8054279e4f23eeb98bd
Kader517777 <shakhabdulkader2020@gmail.com>
2023-12-24 08:08:44 +0600

    first commit

---
+++ src/Route/Route.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router-dom";
+
+export const Route = createBrowserRouter([
+    {
+        path: "/",
+        element: <div>Hello world!</div>,
+    },
+]);
+
